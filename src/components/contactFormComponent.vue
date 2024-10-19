5646efafc0afb542ad63271face6450d31b22a94
ulisescastell <ulises.castell.7e7@itb.cat>
2024-10-19 23:49:26 +0200

    save

--- src/components/contactFormComponent.vue
+++ src/components/contactFormComponent.vue
@@ -26,21 +26,61 @@
 </script>
 
 <template>
+    <div class="container">
+    <h1>Contact book <span class="uli">by Uli</span></h1>
     <main>
         <form>
-            <input type="text" v-model="name" placeholder="Write your name...">
-            <input type="text" v-model="number" placeholder="Write your number...">
+            <input type="text" v-model="name" placeholder="Write your name..." required >
+            <input type="text" v-model="number" placeholder="Write your number..." required >
             <button @click="saveContact">Save contact</button>
-        </form>
-        <input type="text" v-model="contact1" placeholder="Filter contacts">
+            <input type="text" v-model="contact1" placeholder="Filter contacts" class="filt">
         <ul>
             <li v-for="contact in filteredContacts" :key="contact.name"  
             :title="`Contact name: ${contact.name}, Number: ${contact.number}`">
-            {{ contact.name }} - {{ contact.number }}</li>
+            {{ contact.name }} - <span>{{ contact.number }}</span></li>
         </ul>
+        </form>
     </main>
+</div>
 </template>
 
 <style scoped>
+    div {
+        min-height: 90vh;
+    }
 
+    .container {
+        display: grid;
+        place-content: center;
+        flex-direction: column;
+        align-items: center;
+    }
+    main {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        
+    }
+    li {
+        list-style: none;
+        font-size: 20px;
+        color: #61677C;
+
+    }
+
+    span {
+        color: rgb(126, 91, 208);
+        font-weight: bolder;
+    }
+
+    .filt {
+        width: 325px;
+    }
+
+    .uli {
+        color: rgb(126, 91, 208);
+        font-weight: bolder; 
+        font-size: 23px;
+        font-style: italic;
+    }
 </style>
